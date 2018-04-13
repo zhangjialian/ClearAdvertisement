7aac177378260e83cf83f121828502df83d4f5b3
zhang.jialian <zhangjialian@ruhnn.com>
2018-04-13 10:20:06 +0800

    百度页面点击查询时，触发清除动作

--- injection.js
+++ injection.js
@@ -2,44 +2,64 @@
 
 var host = window.location.host;
 
-//总共执行的时间，循环执行，防止部分广告内容未加载出来
-var totalTime = 10;
+clearAdv();
 
-var i = 0;
-setInterval(function(){
-	if(i > totalTime){
-		return;
+/**
+ *
+ */
+$(".bg.s_btn").click(function(){
+	if (isBaidu()){
+		clearAdv();
 	}
+});
 
-    console.log("开始清除广告");
+function isBaidu(){
+    return host.indexOf("baidu") >= 0;
+}
 
-    /**
-     * 百度搜索页广告
-     */
-	if(host.indexOf("baidu") >= 0){
-        $("#content_left>div").each(function(){
-            var self = $(this);
-            //红底，左上角有个广告的链接
-            var advText1 = self.children("a").children("span").text();
-            if(advText1 == "广告"){
-                self.remove();
-                return;
-            }
-            //普通链接，右下角有个广告的链接
-            self.find("a,span").each(function(){
-                if($(this).text() == "广告"){
+/**
+ * 清除广告
+ */
+function clearAdv(){
+	//总共执行的时间，循环执行，防止部分广告内容未加载出来
+    var totalTime = 10;
+
+    var i = 0;
+    setInterval(function(){
+        if(i > totalTime){
+            return;
+        }
+
+        console.log("开始清除广告");
+
+        /**
+         * 百度搜索页广告
+         */
+        if(isBaidu()){
+            $("#content_left>div").each(function(){
+                var self = $(this);
+                //红底，左上角有个广告的链接
+                var advText1 = self.children("a").children("span").text();
+                if(advText1 == "广告"){
                     self.remove();
                     return;
                 }
+                //普通链接，右下角有个广告的链接
+                self.find("a,span").each(function(){
+                    if($(this).text() == "广告"){
+                        self.remove();
+                        return;
+                    }
+                })
+
             })
+        }else{
+            /**
+             * 百度联盟相关网页
+             */
+            $("iframe").hide();
+        }
 
-        })
-	}else{
-        /**
-         * 百度联盟相关网页
-         */
-        $("iframe").hide();
-	}
-
-    i++;
-}, 500);
+        i++;
+    }, 500);
+}
