8b443238d40a511fa3211a52f091a456ac0e015b
zhang.jialian <zhangjialian@ruhnn.com>
2018-04-26 10:59:07 +0800

    代码结构优化，百度知道页面广告清除

--- injection.js
+++ injection.js
@@ -1,69 +1,14 @@
 //$(document).ready(function() {
 
-var host = window.location.host;
 
-clearAdv();
+//进入页面触发清理事件
+ClearAdvUtils.clearAdv();
 
 /**
- * 百度列表页，点击搜索按钮
+ * 百度列表页，点击搜索按钮 触发清理事件
  */
 $(".bg.s_btn").click(function(){
-	if (isBaidu()){
-		clearAdv();
+	if (ClearAdvUtils.isBaiduQuery()){
+        ClearAdvUtils.clearAdv();
 	}
 });
-
-/**
- * 是否百度页面
- * @returns {boolean}
- */
-function isBaidu(){
-    return host.indexOf("baidu") >= 0;
-}
-
-/**
- * 清除广告
- */
-function clearAdv(){
-	//总共执行的次数，循环执行，防止部分广告内容未加载出来
-    var totalTime = 10;
-
-    var i = 0;
-    setInterval(function(){
-        if(i > totalTime){
-            return;
-        }
-
-        console.log("开始清除广告");
-
-        /**
-         * 百度搜索页广告
-         */
-        if(isBaidu()){
-            $("#content_left>div").each(function(){
-                var self = $(this);
-                //红底，左上角有个广告的链接
-                var advText1 = self.children("a").children("span").text();
-                if(advText1 == "广告"){
-                    self.remove();
-                    return;
-                }
-                //普通链接，右下角有个广告的链接
-                self.find("a,span").each(function(){
-                    if($(this).text() == "广告"){
-                        self.remove();
-                        return;
-                    }
-                })
-
-            })
-        }else{
-            /**
-             * 百度联盟相关网页
-             */
-            $("iframe").hide();
-        }
-
-        i++;
-    }, 500);
-}
--- js/config.js
+++ js/config.js
@@ -1,11 +1,68 @@
-var product_host = "http://139.224.164.254";
-var debug_host = "http://localhost";
-var is_debug = false;
+var ClearAdvUtils = {
+    host: window.location.host,
+    /**
+     * 是否百度搜索页面
+     * @returns {boolean}
+     */
+    isBaiduQuery: function (){
+        return this.host.indexOf("www.baidu.com") >= 0;
+    },
+    /**
+     * 是否百度知道
+     * @returns {boolean}
+     */
+    isBaiduZhidao: function(){
+        return this.host.indexOf("zhidao.baidu.com") >= 0;
+    },
+    /**
+     * 清除广告
+     */
+    clearAdv: function (){
+        //总共执行的次数，循环执行，防止部分广告内容未加载出来
+        var totalTime = 10;
+        var i = 0;
+        var self = this;
+        setInterval(function(){
+            if(i > totalTime){
+                return;
+            }
+            self.clearAdvOneTime();
+            i++;
+        }, 500);
+    },
+    clearAdvOneTime: function(){
+        console.log("开始清除广告");
+        /**
+         * 百度搜索页广告
+         */
+        if(this.isBaiduQuery()){
+            $("#content_left>div").each(function(){
+                var self = $(this);
+                //红底，左上角有个广告的链接
+                var advText1 = self.children("a").children("span").text();
+                if(advText1 == "广告"){
+                    self.remove();
+                    return;
+                }
+                //普通链接，右下角有个广告的链接
+                self.find("a,span").each(function(){
+                    if($(this).text() == "广告"){
+                        self.remove();
+                        return;
+                    }
+                })
 
-function getHost(){
-    if(is_debug){
-        return debug_host;
-    }else{
-        return product_host;
+            })
+        } else if(this.isBaiduZhidao()){
+            /**
+             * 百度知道
+             */
+            $(".grid-r.qb-side/*,.widget-new-graphic*/").remove();
+        }else{
+            /**
+             * 百度联盟相关网页
+             */
+            $("iframe").remove();
+        }
     }
-}
+};
